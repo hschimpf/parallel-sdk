be85b655883f8e2fcc7039b79a78f9548ac7d919
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2023-04-03 15:06:03 -0400

    Removed dead code from Scheduler
    * Tasks related attributes were moved to ManagesTasks trait
    * WIP: CPU, memory and ProgressBar related attributes moved to HasSharedProgressBar trait
    * runNextTask() was redesigned and moved to ManagesTasks trait

--- src/Parallel/Scheduler.php
+++ src/Parallel/Scheduler.php
@@ -12,17 +12,14 @@ use HDSSolutions\Console\Parallel\Internals\Task;
 use HDSSolutions\Console\Parallel\Internals\ProgressBarWorker;
 use HDSSolutions\Console\Parallel\Internals\RegisteredWorker;
 use HDSSolutions\Console\Parallel\Internals\Runner;
-use HDSSolutions\Console\Parallel\Internals\Worker;
 use parallel\Channel;
 use parallel\Events\Event\Type;
 use parallel\Future;
 use parallel\Runtime;
 use RuntimeException;
-use Symfony\Component\Console\Helper\Helper;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Throwable;
-use parallel;
 use function parallel\run;
 
 final class Scheduler {
@@ -33,23 +30,11 @@ final class Scheduler {
     /** @var string Unique ID of the instance */
     private string $uuid;
 
-    /** @var RegisteredWorker[] Registered workers */
-    private array $registered_workers = [];
-
     /** @var ?Channel Channel to wait threads start */
     private ?Channel $starter = null;
 
-    /** @var Task[] Collection of pending tasks */
-    private array $pendingTasks = [];
-
     /** @var Future[] Collection of running tasks */
     private array $futures = [];
-
-    /** @var array Collection of results from threads */
-    private array $results = [];
-
-    /** @var ?int Max CPU usage count */
-    private ?int $max_cpu_count = null;
 
     /**
      * @var ProgressBar|null ProgressBar instance for non-threaded Tasks execution
@@ -60,16 +45,6 @@ final class Scheduler {
      * @var bool Flag to identify if ProgressBar is already started (non-threaded)
      */
     private bool $progressBarStarted = false;
-
-    /**
-     * @var array Memory usage stats (non-threaded)
-     */
-    private array $memory_stats = [ 'current' => 0, 'peak' => 0 ];
-
-    /**
-     * @var array Total of items processed per second (non-threaded)
-     */
-    private array $items = [];
 
     /**
      * @var Future|null Thread controlling the ProgressBar
@@ -349,158 +324,6 @@ final class Scheduler {
 
         // wait for all tasks to finish
         while ( !empty(array_filter(self::instance()->futures, static fn(Future $future): bool => !$future->done()))) usleep(10_000);
-    }
-
-    private function runNextTask(): void {
-        // check if there is an available CPU
-        if ( !empty($this->pendingTasks) && $this->hasCpuAvailable()) {
-            // get next available pending task
-            $pending_task = array_shift($this->pendingTasks);
-
-            // create starter channel to wait threads start event
-            $this->starter ??= extension_loaded('parallel') ? Channel::make(sprintf('starter@%s', $this->uuid)) : null;
-
-            // process task inside a thread (if parallel extension is available)
-            if (extension_loaded('parallel')) {
-                // parallel available, process task inside a thread
-                $this->futures[] = run(static function(string $uuid, Task $pending_task): ProcessedTask {
-                    // get registered worker
-                    $registered_worker = $pending_task->getRegisteredWorker();
-                    // get Worker class to instantiate
-                    $worker_class = $registered_worker->getWorkerClass();
-
-                    /** @var ParallelWorker $worker Instance of the Worker */
-                    $worker = new $worker_class(...$registered_worker->getArgs());
-                    // build task params
-                    $params = $worker instanceof Worker
-                        // process task using local Worker
-                        ? [ $registered_worker->getClosure(), ...$pending_task->getData() ]
-                        // process task using user Worker
-                        : [ ...$pending_task->getData() ];
-
-                    // check if worker has ProgressBar enabled
-                    if ($registered_worker->hasProgressEnabled()) {
-                        // connect worker to ProgressBar
-                        $worker->connectProgressBar($uuid, $GLOBALS['worker_thread_id'] ??= sprintf('%s@%s', $uuid, substr(md5(uniqid($worker_class, true)), 0, 16)));
-                    }
-
-                    // notify that thread started
-                    Channel::open(sprintf('starter@%s', $uuid))->send(true);
-
-                    // process task
-                    $worker->start(...$params);
-
-                    // return Worker result
-                    return $worker->getProcessedTask();
-                }, [
-                    // send UUID for starter channel
-                    $this->uuid,
-                    // send pending task to process
-                    $pending_task,
-                ]);
-
-            } else {
-                // get registered worker
-                $registered_worker = $pending_task->getRegisteredWorker();
-                // get Worker class to instantiate
-                $worker_class = $registered_worker->getWorkerClass();
-
-                /** @var ParallelWorker $worker Instance of the Worker */
-                $worker = new $worker_class(...$registered_worker->getArgs());
-                // build task params
-                $params = $worker instanceof Worker
-                    // process task using local Worker
-                    ? [ $registered_worker->getClosure(), ...$pending_task->getData() ]
-                    // process task using user Worker
-                    : [ ...$pending_task->getData() ];
-
-                // check if worker has ProgressBar enabled
-                if ($registered_worker->hasProgressEnabled()) {
-                    // connect worker to ProgressBar
-                    $worker->connectProgressBar(function(string $action, array $args) {
-                        // update stats
-                        if ($action === 'advance') {
-                            // count processed item
-                            $this->items[ time() ] = ($this->items[ time() ] ?? 0) + 1;
-                        }
-                        // update ProgressBar memory usage report
-                        $this->progressBar->setMessage($this->getMemoryUsage(), 'threads_memory');
-                        // update ProgressBar items per second report
-                        $this->progressBar->setMessage($this->getItemsPerSecond(), 'items_per_second');
-
-                        // execute progress bar action
-                        $this->progressBar->$action(...$args);
-                    });
-                }
-
-                // process task using worker
-                $worker->start(...$params);
-
-                // store Worker result
-                $this->futures[] = $worker->getProcessedTask();
-            }
-
-            // wait for thread to start
-            if (($this->starter?->recv() ?? true) !== true) {
-                throw new RuntimeException('Failed to start Task');
-            }
-        }
-    }
-
-    private function getMemoryUsage(): string {
-        // update memory usage for this thread
-        $this->memory_stats['current'] = memory_get_usage(true);
-        // update peak memory usage
-        if ($this->memory_stats['current'] > $this->memory_stats['peak']) {
-            $this->memory_stats['peak'] = $this->memory_stats['current'];
-        }
-
-        // current memory used
-        $main = Helper::formatMemory($this->memory_stats['current']);
-        // peak memory usage
-        $peak = Helper::formatMemory($this->memory_stats['peak']);
-
-        return "$main, ↑ $peak";
-    }
-
-    private function getItemsPerSecond(): string {
-        // check for empty list
-        if (empty($this->items)) return '0';
-
-        // keep only last 15s for average
-        $this->items = array_slice($this->items, -15, preserve_keys: true);
-
-        // return the average of items processed per second
-        return '~'.number_format(floor(array_sum($this->items) / count($this->items) * 100) / 100, 2);
-    }
-
-    private function getMaxCpuUsage(): int {
-        // return configured max CPU usage
-        return $this->max_cpu_count ??= (isset($_SERVER['PARALLEL_MAX_COUNT']) ? (int) $_SERVER['PARALLEL_MAX_COUNT'] : cpu_count( (float) ($_SERVER['PARALLEL_MAX_PERCENT'] ?? 1.0) ));
-    }
-
-    private function hasCpuAvailable(): bool {
-        // return if there is available CPU
-        return count($this->futures) < $this->getMaxCpuUsage();
-    }
-
-    private function cleanFinishedTasks(): void {
-        // release finished tasks from futures
-        $finished_tasks = [];
-        foreach ($this->futures as $idx => $future) {
-            // check if future is already done working
-            if ( !extension_loaded('parallel') || $future->done()) {
-                // store the ProcessedTask
-                try { $this->results[] = extension_loaded('parallel') ? $future->value() : $future;
-                } catch (Throwable) {}
-
-                // add future idx to finished tasks list
-                $finished_tasks[] = $idx;
-            }
-        }
-
-        // remove finished tasks from futures
-        foreach ($finished_tasks as $idx) unset($this->futures[$idx]);
     }
 
     /**
